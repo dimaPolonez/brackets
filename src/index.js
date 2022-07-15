039a27d6c671157058439f2417eb7533be766f8b
dimaPolonez <dima-barxat@yandex.ru>
2022-07-15 19:47:26 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,59 +1,26 @@
-/* module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
 
-let arrString = [];
-let arrOpen = [];
-
-arrString = str.split('');
-
-if (arrString.lentg % 2 != 0) {
-  return false;
-} else {
-  arrString.forEach(function (value) {
-    for (i = 0; i < bracketsConfig.length; i++) {
-      if (bracketsConfig[i].includes(value) === true) {
-        arrOpen.push(String(i));
-      }
-    }
+  let strNew = str;
+  let bracketNew = [];
+  let lenOld = null;
+  
+  bracketsConfig.forEach( (value) => {
+    bracketNew.push(value.join(''));
   });
-  arrOpen.forEach((value) => {
-    if (value === value + 2) {
-      return false;
+  
+  if (strNew.length % 2 !== 0) {
+    return false;
+  } else {
+    while (strNew.length != 0){
+      bracketNew.forEach((value) => {
+        strNew = strNew.replace(value, '');
+      })
+      if (strNew.length === lenOld){
+        break;
+      }
+      lenOld = strNew.length;
     }
-  })
-}
-} */
-
-/* const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];  */
-
-
-
-const str = '([{}])';
-const bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
-
-let strNew = str;
-let subStr = '';
-let bracketNew = [];
-bracketsConfig.forEach( (value) => {
-  bracketNew.push(value.join(''));
-});
-
-if (strNew.length % 2 !== 0) {
-  return false;
-} else {
-  while (strNew.length != 0){
-    bracketNew.forEach((value) => {
-      strNew.replace(value, '');
-      console.log(strNew);
-      console.log(strNew.length);
-    })
-  }
+    return (strNew.length === 0);
+  } 
+  
 } 
-console.log(bracketNew);
-
-
